197d3d3360e3190c66a67b814a6989fc89ab56c0
Will Hart <hart.wl@gmail.com>
2020-07-10 19:25:21 +1000

    Typescript dev tweaks

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,20 @@
-import svelte from "rollup-plugin-svelte";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
-import typescript from "@rollup/plugin-typescript";
+import commonjs from '@rollup/plugin-commonjs'
+import html from 'rollup-plugin-html2'
+import livereload from 'rollup-plugin-livereload'
+import resolve from '@rollup/plugin-node-resolve'
+import svelte from 'rollup-plugin-svelte'
+import { terser } from 'rollup-plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH
 
 export default {
-  input: "src/main.js",
+  input: 'src/main.js',
   output: {
     sourcemap: true,
-    format: "iife",
-    name: "app",
-    file: "public/build/bundle.js",
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js',
   },
   plugins: [
     svelte({
@@ -22,9 +23,12 @@ export default {
       // we'll extract any component CSS out into
       // a separate file - better for performance
       css: (css) => {
-        css.write("public/build/bundle.css");
+        css.write('public/build/bundle.css')
       },
+      extensions: ['.svelte'],
     }),
+
+    typescript(),
 
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
@@ -33,10 +37,14 @@ export default {
     // https://github.com/rollup/plugins/tree/master/packages/commonjs
     resolve({
       browser: true,
-      dedupe: ["svelte"],
+      dedupe: ['svelte'],
     }),
-    typescript(),
     commonjs(),
+
+    html({
+      template: 'public/index.html',
+      fileName: 'index2.html',
+    }),
 
     // In dev mode, call `npm run start` once
     // the bundle has been generated
@@ -44,7 +52,7 @@ export default {
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
-    !production && livereload("public"),
+    !production && livereload('public'),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
@@ -53,21 +61,21 @@ export default {
   watch: {
     clearScreen: false,
   },
-};
+}
 
 function serve() {
-  let started = false;
+  let started = false
 
   return {
     writeBundle() {
       if (!started) {
-        started = true;
+        started = true
 
-        require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
-          stdio: ["ignore", "inherit", "inherit"],
+        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+          stdio: ['ignore', 'inherit', 'inherit'],
           shell: true,
-        });
+        })
       }
     },
-  };
+  }
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,15 @@
+<svelte:options immutable={true} />
+
 <script>
+
   import { timer } from './timer'
+  import produce from 'immer'
+  import testStore from './stores/testStore'
+  const { actions, subscribe: test } = testStore
 
-  let count = 0
-
-  $: if (count >= 10) {
-    alert(`count is dangerously high!`)
-    count = 9
-  }
-
-  function handleClick() {
-    count += 1
-  }
 </script>
 
   time: {$timer}s
-<button on:click={handleClick}>
-  Clicked {count} {count === 1 ? 'time' : 'times'}
+<button on:click={actions.test}>
+  Clicked {$testStore.tested} times
 </button>
